522b22ed9d24f3b48c4a3986269eb2a364d16c98
Cristian Lopez <cristian@getrentcheck.com>
2025-02-11 17:28:38 -0300

    Working version with GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -12,6 +15,10 @@ import { UsersModule } from './users/users.module';
       expandVariables: true,
     }),
     UsersModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.gql',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
