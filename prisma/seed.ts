bc8edf19a1da1702728fae2f845b37ab8a45246d
Cristian Lopez <cristian@getrentcheck.com>
2025-02-26 21:28:09 -0300

    adding user password and seed, init auth resources

---
+++ prisma/seed.ts
@@ -0,0 +1,39 @@
+import * as bcrypt from 'bcrypt';
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const saltRounds = 10;
+
+async function main() {
+  const adminCredentials = {
+    username: 'admin',
+    email: 'admin@vandelay-labs.com',
+    password: '13245678',
+  };
+
+  const hashedPassword = await bcrypt.hash(
+    adminCredentials.password,
+    saltRounds,
+  );
+
+  await prisma.user.upsert({
+    where: { email: adminCredentials.email },
+    update: {},
+    create: {
+      username: adminCredentials.username,
+      email: adminCredentials.email,
+      password: hashedPassword,
+    },
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
